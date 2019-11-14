a1f5b3c6335736442c7b833e683bfae4f0e634bf
Barry089 <lizhe_email@163.com>
2019-11-14 14:49:10 +0800

    threading.Thread()

---
+++ threading.Thread.py
@@ -0,0 +1,14 @@
+import threading
+
+
+class mythread(threading.Thread):
+    def run(self):
+        for i in range(1, 10):
+            print(i)
+
+
+thread1 = mythread()
+thread2 = mythread()
+
+thread1.start()
+thread2.start()
