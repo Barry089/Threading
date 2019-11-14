fd4574577bf0bcbaabab7279a00cddbd03004078
Barry089 <lizhe_email@163.com>
2019-11-14 15:44:47 +0800

    主线程

---
+++ 主线程.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+
+def test():
+    time.sleep(10)
+    for i in range(1, 10):
+        print(i)
+
+
+thread1 = threading.Thread(target=test, daemon=False)
+thread1.start()
+
+print("主线程完了")
